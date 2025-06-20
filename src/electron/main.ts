a655f87cc3e88176eb9258c718e20edc66e2127d
Rafael Rios <rvrios22@gmail.com>
2025-06-20 06:29:41 -0700

    add: tanstack routing

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -2,6 +2,10 @@ import { app, BrowserWindow } from 'electron'
 import path from 'path'
 import isDev from './utils/isDev.js'
 import { getPreloadPath } from './utils/pathResolver.js'
+import os from 'os';
+if (os.platform() === 'darwin' && os.arch() === 'x64') {
+  app.disableHardwareAcceleration();
+}
 app.on('ready', () => {
     const mainWindow = new BrowserWindow({
         webPreferences: {
